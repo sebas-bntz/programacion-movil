57f89bf1968fb3ada76061e2a77207b57603c61b
Sebastian Benitez <sebasbenitez07@hotmail.com>
2021-09-14 10:47:30 -0600

    Registro de invitados

---
+++ Practica2/javascript.js
@@ -0,0 +1,18 @@
+var guests = [];
+
+function alerta(){
+    var name = document.getElementById("name").value
+    var age = document.getElementById("age").value
+
+    if (name === "" || age === null) {
+        alert("Llena correctamente los campos")
+    } else {
+        guests.push(name);
+
+        var list = "<li>" + guests.join("</li><li>") + "</li>";
+        document.getElementById("list").innerHTML = list;
+
+        var welcome = "Hola, Bienvenido " + name + " tienes: " + age + " años ";
+        alert(welcome);
+    }
+}
